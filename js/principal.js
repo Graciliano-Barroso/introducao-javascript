9bbe4533950c63c6d24c6dd4da8826f438e20bd2
Rico <grico484@gmail.com>
2022-10-02 19:22:25 -0400

    Calculando imc do primeiro paciente

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,16 @@
 let titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+let paciente = document.querySelector("#primeiro-paciente");
+
+let tdPeso = document.querySelector(".info-peso");
+let peso = tdPeso.textContent;
+
+let tdAltura = document.querySelector(".info-altura");
+let altura = tdAltura.textContent;
+
+let tdImc = document.querySelector(".info-imc");
+
+let imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
